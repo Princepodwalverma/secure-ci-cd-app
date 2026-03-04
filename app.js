c09ac6dc4315dd00c9b7e57ee63aeec5b306844b
Prince Verma <princepodwalverma@gmail.com>
2026-03-04 21:28:20 +0530

    Fix XSS vulnerability

--- app.js
+++ app.js
@@ -1,19 +1,21 @@
 const express = require("express");
+const validator = require("validator");
+
 const app = express();
+const PORT = 4000;
 
-// 🔥 Hardcoded secret (SonarQube detect karega)
-const API_SECRET = "mySuperSecretPassword123";
-
-// Vulnerable endpoint (no validation)
+// Secure endpoint
 app.get("/user", (req, res) => {
-  const username = req.query.name;
-  res.send("Hello " + username);
+  let username = req.query.name || "Guest";
+
+  // Input sanitization
+  if (!validator.isAlphanumeric(username)) {
+    return res.status(400).send("Invalid username");
+  }
+
+  res.send(`Hello ${username}`);
 });
 
-app.get("/", (req, res) => {
-  res.send("Secure CI/CD Demo App Running");
-});
-
-app.listen(4000, () => {
-  console.log("App running on port 4000");
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
